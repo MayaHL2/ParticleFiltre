3466034fefdc4db8ef84f6b47d2cdb118b76271d
Maya HL <hadjlazibmaya15@gmail.com>
2021-12-10 15:53:27 +0100

    robot can walk perfectly over the room

---
+++ particle_filter_robot.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+# coding: utf-8
+ 
+# In[2]:
+
+
+import numpy as np 
+import cv2
+
+map = cv2.imread("map.png", 0)
+
+#percent by which the image is resized
+scale_percent = 300
+
+#calculate the 50 percent of original dimensions
+width = int(map.shape[1] * scale_percent / 100)
+height = int(map.shape[0] * scale_percent / 100)
+
+# dsize
+dsize = (width, height)
+
+# resize image
+map = cv2.resize(map, dsize)
+cv2.imshow("map_i", map)
+
+HEIGHT, WIDTH = map.shape
+
+rx, ry, rtheta = (WIDTH/4, HEIGHT/4, 0)
+
+
+# In[3]:
+
+
+STEP = 5
+TURN = np.radians(25)
+def get_input():
+  fwd = 0
+  turn = 0
+  halt = 0
+  k = cv2.waitKey(0)&0xFF
+
+
+  if k == 109:
+    fwd = STEP
+  elif k == 33:
+    turn = TURN
+  elif k == 58:
+    turn = -TURN
+  else: 
+    halt = True
+
+  return fwd, turn, halt
+
+
+# In[4]:
+
+
+SIGMA_STEP = 0.5
+SIGMA_TURN = np.radians(5)
+
+def move_robots(rx, ry, rtheta, fwd, turn):
+  fwd_noisy = np.random.normal(fwd, SIGMA_STEP, 1)
+  rx += fwd_noisy*np.cos(rtheta)
+  ry += fwd_noisy*np.sin(rtheta)
+
+  turn_noisy = np.random.normal(turn, SIGMA_TURN, 1)
+  rtheta += turn_noisy
+  return rx, ry, rtheta
+
+
+# In[5]:
+
+
+NUM_PARTICLES = 3000
+
+def init():
+  particles = np.random.rand(NUM_PARTICLES, 3)
+  particles *= np.array((WIDTH, HEIGHT, np.radians(360)))
+  return particles
+
+
+# In[6]:
+
+
+def move_particles(particles, fwd, turn):
+  particles[:, 0] += fwd*np.cos(rtheta)
+  particles[:, 1] += fwd*np.sin(rtheta)
+  particles[:, 2] += turn
+
+  particles[:, 0] = np.clip(particles[:, 0], 0.0, WIDTH-1)
+  particles[:, 1] = np.clip(particles[:, 1], 0.0, HEIGHT-1)
+
+  return particles
+
+
+# In[7]:
+
+
+SIGMA_SENSOR = 2
+
+def sense(x, y, noisy = False):
+  x = int(x)
+  y = int(y)
+  if noisy: 
+    return np.random.normal(map[y,x], SIGMA_SENSOR, 1)
+  return map[y,x]
+
+
+# In[8]:
+
+
+def compute_weights(particles, robot_sensor):
+  errors = np.zeros(NUM_PARTICLES)
+  for i in range(NUM_PARTICLES):
+    elevation = sense(particles[i,0], particles[i,1], noisy = False)
+    errors[i] = abs(robot_sensor - elevation)
+  weights = np.max(errors) - errors
+
+  weights[np.where(
+            (particles[:,0] == 0)|
+            (particles[:,0] == WIDTH-1)|
+            (particles[:,1] == 0)|
+            (particles[:,1] == HEIGHT-1)
+          )[0]
+          ] = 0.0
+
+  weights = weights**3
+  return weights
+
+
+# In[9]:
+
+
+def resample(particles, weights):
+  probabilities = weights/np.sum(weights)
+  new_index = np.random.choice(
+      NUM_PARTICLES, 
+      size = NUM_PARTICLES, 
+      p = probabilities
+  )
+
+  particles = particles[new_index, :]
+
+  return particles
+
+
+# In[10]:
+
+
+SIGMA_POS = 2
+SIGMA_TURN = np.radians(10)
+def add_noise(particles):
+  noise = np.concatenate(
+      (
+          np.random.normal(0,SIGMA_POS, (NUM_PARTICLES, 1)),
+          np.random.normal(0,SIGMA_POS, (NUM_PARTICLES, 1)),
+          np.random.normal(0,SIGMA_TURN, (NUM_PARTICLES, 1))  
+      ),
+      axis = 1
+  )
+  particles += noise 
+  return particles
+
+
+# In[1]:
+
+
+def display(map, rx, ry, particles):
+  lmap = cv2.cvtColor(map, cv2.COLOR_GRAY2BGR)
+
+  # Display particles
+  if len(particles) > 0:
+    for i in range(NUM_PARTICLES):
+      cv2.circle(lmap, 
+                 (int(particles[i,0]),(int(particles[i,1]))),
+                  1, 
+                  (255, 0, 0),
+                  1)
+                 
+  # Display robot 
+  cv2.circle(lmap, (int(rx), int(ry)), 5, (0,255,0), 10)
+
+  # Display best guess
+  if len(particles) > 0:
+    px = np.mean(particles[:,0])
+    py = np.mean(particles[:,1])
+    cv2.circle(lmap, (int(px), int(py)), 5, (0,0,255), 5)
+  
+  cv2.imshow("map", lmap)
+
+
+# In[14]:
+
+
+particles = init()
+
+while True:
+    display(map, rx, ry, particles)
+    fwd, turn, halt = get_input()
+    if halt: 
+        break
+    rx, ry, rtheta = move_robots(rx, ry, rtheta, fwd, turn) 
+    particles = move_particles(particles, fwd, turn)
+    if fwd != 0:
+        robot_sensor = sense(rx, ry, noisy = True)
+        weights = compute_weights(particles, robot_sensor)
+        particles = resample(particles, weights)
+        particles = add_noise(particles)
+
+cv2.destroyAllWindows()
---
+++ particle_filter_room.py
@@ -0,0 +1,222 @@
+import numpy as np 
+import cv2
+from sewar.full_ref import mse
+import time
+
+def gasuss_noise(image, mean=0, var=0.000001):
+
+    image = np.array(image/255, dtype=float)
+    noise = np.random.normal(mean, var ** 0.5, image.shape)
+    out = image + noise
+    if out.min() < 0:
+        low_clip = -1.
+    else:
+        low_clip = 0.
+    out = np.clip(out, low_clip, 1.0)
+    out = np.uint8(out*255)
+    
+    kernel = np.ones((5,5),np.float32)/25
+    out = cv2.filter2D(out, -1, kernel)
+    #cv.imshow("gasuss", out)
+    return out
+
+def create_room(door, thikness = 10):
+    fond = cv2.imread("fond.png")
+    HEIGHT, WIDTH,_ = fond.shape
+    room = cv2.line(fond, (0,0), (0,HEIGHT), (0,0,0), thikness)
+    room = cv2.line(room, (0,0), (WIDTH,0), (0,0,0), thikness)
+    room = cv2.line(fond, (WIDTH,HEIGHT), (0,HEIGHT), (0,0,0), thikness)
+    room = cv2.line(room, (WIDTH,HEIGHT), (WIDTH,0), (0,0,0), thikness)
+
+    if door:
+        door_1 = np.random.randint(2*thikness, HEIGHT-5*thikness)
+        door_2 = np.random.randint(door_1+3*thikness, HEIGHT-2*thikness)
+        # print(door_1, door_2, HEIGHT)
+        room = cv2.line(room, (WIDTH,door_1), (WIDTH,door_2), (255,255,255), thikness)
+
+    return room
+    # cv2.imshow("room", room)
+
+
+room = create_room(door = True)
+HEIGHT, WIDTH,_ = room.shape
+print(HEIGHT, WIDTH)
+
+# The position of the robot is relative to the room
+# its rotation is relative to its center with x,y <=> width, height
+def put_robot(room, pos, rot_angle, robot_radius = 25, thikness = 10):
+    # lroom = cv2.cvtColor(room, cv2.COLOR_GRAY2BGR)
+    rot_angle_rad = np.radians(rot_angle)
+    cv2.circle(room, pos, robot_radius, (255,0,0), -1)
+    cv2.line(room, pos, (int(pos[0]+robot_radius*np.cos(rot_angle)), int(pos[1]+robot_radius*np.sin(rot_angle))),(0,0,0), thikness//2)
+    # cv2.imshow("room with robot", room)
+    return room
+
+
+
+
+def get_input(TURN = np.radians(5), STEP = 5):
+  fwd = 0
+  turn = 0
+  halt = False
+  k = cv2.waitKey(0)&0xFF
+
+  if k == 109:
+    fwd = STEP
+  elif k == 33:
+    turn = TURN
+  elif k == 58:
+    turn = -TURN
+  elif k == 0:
+    halt = True
+
+  return fwd, turn, halt
+
+
+
+
+def move(pos, rot_angle, fwd, turn, noisy = True, STD_STEP = 2, STD_TURN = np.radians(3)):
+  if noisy: 
+      fwd_noisy = np.random.normal(fwd, STD_STEP, 1)
+      turn_noisy = np.random.normal(turn, STD_TURN, 1)
+  else: 
+      fwd_noisy = fwd
+      turn_noisy = turn
+
+  pos0 = pos[0]+int(fwd_noisy*np.cos(rot_angle))
+  pos1 = pos[1]+int(fwd_noisy*np.sin(rot_angle))
+  
+  rot_angle += turn_noisy
+  return (pos0, pos1), rot_angle
+
+
+def sense_all_around(room, pos, rot_angle, noisy, sensor_angle = 30, max_distance_sensor = 150):
+    # rot_angle_rad = np.radians(rot_angle)
+    sensor_angle_rad = np.radians(sensor_angle)
+    max_ = int(max_distance_sensor/np.cos(sensor_angle_rad/2))
+
+    mask = np.zeros((2*(max_), 2*(max_)))
+    cv2.circle(mask, (mask.shape[1]//2, mask.shape[0]//2), (max_), (255,255,255), -1)
+    # cv2.imshow("mask", mask)
+    
+    # M = cv2.getRotationMatrix2D(pos, rot_angle, 1)
+    # lroom = cv2.warpAffine(lroom, M, (lroom.shape[1], lroom.shape[0]))
+ 
+    sensor = cv2.copyMakeBorder(room, max_, max_, max_, max_,  cv2.BORDER_CONSTANT, value = (0, 0, 0))
+    
+    if noisy:
+        x = np.random.randint(0, 5)
+        y = np.random.randint(0, 5)
+        # theta = np.random.randn()*3
+        sensor = sensor[pos[1] + y:pos[1]+2*max_+ y, pos[0] +x:pos[0]+2*max_+x,:]
+        # M = cv2.getRotationMatrix2D(pos, theta, 1)
+        # print(theta)
+        PEAK = np.random.randint(850000,1000000)/1000000
+        sensor = gasuss_noise(cv2.cvtColor(sensor, cv2.COLOR_BGR2GRAY))
+        sensor = np.random.poisson(sensor / 255.0 * PEAK) / PEAK * 255
+        cv2.circle(sensor, (sensor.shape[1]//2, sensor.shape[0]//2), sensor.shape[1], (0,0,0), sensor.shape[1])
+        # print(np.shape(sensor))
+        # sensor = cv2.warpAffine(sensor, M, (sensor.shape[1], sensor.shape[0]))
+        
+    else:
+        sensor = cv2.cvtColor(sensor, cv2.COLOR_BGR2GRAY)
+        sensor = sensor[pos[1]:pos[1]+2*max_, pos[0]:pos[0]+2*max_]
+        cv2.circle(sensor, (sensor.shape[1]//2, sensor.shape[0]//2), sensor.shape[1], (0,0,0), sensor.shape[1])
+        sensor = sensor.astype(np.float64)
+
+    # cv2.imshow("room", sensor)
+    M = cv2.getRotationMatrix2D((sensor.shape[1]//2, sensor.shape[0]//2), rot_angle, 1)
+    sensor = cv2.warpAffine(sensor, M, (sensor.shape[1], sensor.shape[0]))
+
+    return sensor
+
+
+def sense_lidar(room, pos, rot_angle, noisy, step_angle = 1, step_xy = 5, sensor_angle = 30, max_distance_sensor = 50):
+    rot_angle_rad = np.radians(rot_angle)
+    sensor_angle_rad = np.radians(sensor_angle)
+
+    lroom = cv2.cvtColor(room, cv2.COLOR_BGR2GRAY)
+    
+
+    posx = pos[0]
+    posy = pos[1]
+
+    print(posx, posy)
+
+    pix = lroom[posx, posy]
+    dist = 0 
+    i = 0
+
+    while pix>0 and dist<max_distance_sensor and posx< lroom.shape[1] and posy< lroom.shape[0]:
+        posx += int(step_xy*np.cos(rot_angle_rad-sensor_angle_rad/2 + step_angle*i))
+        posy += int(step_xy*np.sin(rot_angle_rad-sensor_angle_rad/2 + step_angle*i))
+
+        dist += step_xy
+
+        pix = lroom[posx, posy]
+
+        cv2.circle(room, (int(posx), int(posy)), 5, (0,255,255), -1)
+
+    # print(dist)
+
+    
+
+    # return dist
+
+
+    
+
+
+
+def init_particles(num_particles = 3000):
+    particles = np.random.rand(num_particles, 3)
+    particles *= np.array((WIDTH, HEIGHT, np.radians(360)))
+    return particles
+
+
+
+def move_particles(particles, fwd, turn):
+    particles[:, 0] += fwd*np.cos(particles[:, 2])
+    particles[:, 1] += fwd*np.sin(particles[:, 2])
+    particles[:, 2] += turn
+
+    particles[:, 0] = np.clip(particles[:, 0], 0.0, WIDTH-1)
+    particles[:, 1] = np.clip(particles[:, 1], 0.0, HEIGHT-1)
+
+    return particles
+
+
+def put_particles(room, particles, num_particles = 3000):
+    if len(particles) > 0:
+        for i in range(num_particles):
+            cv2.circle(room, (int(particles[i,0]),(int(particles[i,1]))),1, (255, 0, 0), 1)
+
+
+def get_weights(room, sensor_robot, particles):
+    weights = np.zeros(len(particles))
+    for i in range(len(particles)):
+        sensor_particle = sense_lidar(room, (int(particles[i][0]), int(particles[i][1])), particles[i][2], noisy = False)
+        cv2.resize(sensor_particle, (31,31))
+        weights[i] = mse(sensor_robot, sensor_particle)
+    return(weights)
+
+
+halt = False     
+pos = (WIDTH//2, HEIGHT//2)
+rot = 0
+fwd, turn = 0, 0
+
+
+while not(halt):
+
+    lroom = np.array(room)
+
+    fwd, turn, halt = get_input()
+    pos, rot = move(pos, rot, fwd, turn, noisy = True)
+    put_robot(lroom, pos, rot)
+
+    cv2.imshow("room", lroom)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
